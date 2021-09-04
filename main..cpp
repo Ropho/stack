cbdaf2a0fb5f2b52287d2645f1340a9bc1f0cded
Ropho <89077943+Ropho@users.noreply.github.com>
2021-09-04 10:37:40 +0300

    Add files via upload

---
+++ main..cpp
@@ -0,0 +1,65 @@
+#include "stdio.h"
+#include "malloc.h"
+#include "assert.h"
+
+typedef struct my_stack {
+
+    int inf = NULL;
+    struct my_stack *pointer = nullptr;
+} my_stack;
+
+
+void push (my_stack **stack1, int value) {
+
+    my_stack *new_stack = (my_stack*)malloc(sizeof(my_stack));
+
+    if (*stack1 == nullptr) {
+
+        *stack1 = new_stack;
+        (*stack1)->inf = value;
+        (*stack1)->pointer = nullptr;
+
+        return;
+    }
+
+    else {
+
+        new_stack->pointer = *stack1;
+        (*stack1) = new_stack;
+        (*stack1)->inf = value;
+
+        return;
+    }
+}
+
+int pop (my_stack **stack1) {
+
+    assert (*stack1 != nullptr);
+
+    int value = (*stack1)->inf;
+    *stack1 = (*stack1)->pointer;
+    return value;
+}
+
+
+
+
+int main (void) {
+
+    my_stack *kek = nullptr;
+
+    push (&kek, 2);
+    push (&kek, 3);
+
+
+    my_stack *print = kek;
+    while (print != nullptr) {
+
+        printf ("%d ", print->inf);
+        print = print->pointer;
+    }
+
+    printf ("%d\n",pop (&kek));
+    printf ("%d",pop (&kek));
+
+}
